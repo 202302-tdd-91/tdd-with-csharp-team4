e1bf416563d3c1a7226f08a9665218256fbd9249
Joey Chen <jay.hatelove@gmail.com>
2023-02-11 22:19:51 +0800

    add method: Budget.GetDays() for startMonthDays

--- tdd-with-csharp/Budget.cs
+++ tdd-with-csharp/Budget.cs
@@ -1,7 +1,19 @@
-﻿namespace tdd_with_csharp;
+﻿#region
+
+using System;
+
+#endregion
+
+namespace tdd_with_csharp;
 
 public class Budget
 {
-    public string YearMonth { get; set; }
     public int Amount { get; set; }
+    public string YearMonth { get; set; }
+
+    public int GetDays()
+    {
+        var firstDay = DateTime.ParseExact(YearMonth, "yyyyMM", null);
+        return DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
+    }
 }
--- tdd-with-csharp/BudgetService.cs
+++ tdd-with-csharp/BudgetService.cs
@@ -32,7 +32,8 @@ public class BudgetService
                 {
                     if (currentMonth.ToString("yyyyMM") == start.ToString("yyyyMM"))
                     {
-                        var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+                        var startMonthDays = budget.GetDays();
+                        // var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
                         var dailyAmount = budget.Amount / startMonthDays;
 
                         var overlappingDays = startMonthDays - start.Day + 1;
