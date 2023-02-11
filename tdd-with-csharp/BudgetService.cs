bf0d9d41eeaa419ff0f6c8b6aeca9c82100a24bf
Joey Chen <jay.hatelove@gmail.com>
2023-02-11 22:45:54 +0800

    clean up

--- tdd-with-csharp/BudgetService.cs
+++ tdd-with-csharp/BudgetService.cs
@@ -1,8 +1,6 @@
 ﻿#region
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 #endregion
 
@@ -20,28 +18,13 @@ public class BudgetService
     public double Query(DateTime start, DateTime end)
     {
         var budgets = _budgetRepo.GetAll();
-        var currentMonth = start;
         var sum = 0;
         var period = new Period(start, end);
         foreach (var budget in budgets)
         {
-            // }
-            // while (currentMonth < new DateTime(end.Year, end.Month, 1).AddMonths(1))
-            // {
-            //     var budget = GetBudget(budgets, currentMonth.ToString("yyyyMM"));
-            //     if (budget != null)
-            //     {
             sum += budget.GetOverlappingAmount(period);
-            // }
-            //
-            // currentMonth = currentMonth.AddMonths(1);
         }
 
         return sum;
-    }
-
-    private static Budget? GetBudget(List<Budget> budgets, string yearMonth)
-    {
-        return budgets.FirstOrDefault(b => b.YearMonth == yearMonth);
     }
 }
