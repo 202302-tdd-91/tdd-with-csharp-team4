3df5c65a579d30e9dfb62016df57da4a99b608db
Joey Chen <jay.hatelove@gmail.com>
2023-02-11 22:44:25 +0800

    使用同一個 while loop 來同時涵蓋查詢同一個月與跨月的情況

--- tdd-with-csharp/BudgetService.cs
+++ tdd-with-csharp/BudgetService.cs
@@ -20,35 +20,49 @@ public class BudgetService
     public double Query(DateTime start, DateTime end)
     {
         var budgets = _budgetRepo.GetAll();
-
-        if (start.ToString("yyyyMM") != end.ToString("yyyyMM"))
+        var currentMonth = start;
+        var sum = 0;
+        var period = new Period(start, end);
+        while (currentMonth < new DateTime(end.Year, end.Month, 1).AddMonths(1))
         {
-            var currentMonth = start;
-            var sum = 0;
-            var period = new Period(start, end);
-            while (currentMonth < new DateTime(end.Year, end.Month, 1).AddMonths(1))
+            var budget = GetBudget(budgets, currentMonth.ToString("yyyyMM"));
+            if (budget != null)
             {
-                var budget = GetBudget(budgets, currentMonth.ToString("yyyyMM"));
-                if (budget != null)
-                {
-                    sum += budget.GetOverlappingAmount(period);
-                }
-
-                currentMonth = currentMonth.AddMonths(1);
+                sum += budget.GetOverlappingAmount(period);
             }
 
-            return sum;
+            currentMonth = currentMonth.AddMonths(1);
         }
-        else
-        {
-            var oneMonthBudget = GetBudget(budgets, start.ToString("yyyyMM"));
-            if (oneMonthBudget == null) return 0;
 
-            var amount = oneMonthBudget.Amount;
-            var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
-            var amountPerDay = amount / startMonthDays;
-            return amountPerDay * ((end - start).Days + 1);
-        }
+        return sum;
+        // if (start.ToString("yyyyMM") != end.ToString("yyyyMM"))
+        // {
+        //     var currentMonth = start;
+        //     var sum = 0;
+        //     var period = new Period(start, end);
+        //     while (currentMonth < new DateTime(end.Year, end.Month, 1).AddMonths(1))
+        //     {
+        //         var budget = GetBudget(budgets, currentMonth.ToString("yyyyMM"));
+        //         if (budget != null)
+        //         {
+        //             sum += budget.GetOverlappingAmount(period);
+        //         }
+        //
+        //         currentMonth = currentMonth.AddMonths(1);
+        //     }
+        //
+        //     return sum;
+        // }
+        // else
+        // {
+        //     var oneMonthBudget = GetBudget(budgets, start.ToString("yyyyMM"));
+        //     if (oneMonthBudget == null) return 0;
+        //
+        //     var amount = oneMonthBudget.Amount;
+        //     var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+        //     var amountPerDay = amount / startMonthDays;
+        //     return amountPerDay * ((end - start).Days + 1);
+        // }
     }
 
     private static Budget? GetBudget(List<Budget> budgets, string yearMonth)
