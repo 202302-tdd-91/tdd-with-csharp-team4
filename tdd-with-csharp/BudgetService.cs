058d68fff8ffd17b39de77f753d051fca35fe04c
Joey Chen <jay.hatelove@gmail.com>
2023-02-11 22:44:40 +0800

    clean up

--- tdd-with-csharp/BudgetService.cs
+++ tdd-with-csharp/BudgetService.cs
@@ -35,34 +35,6 @@ public class BudgetService
         }
 
         return sum;
-        // if (start.ToString("yyyyMM") != end.ToString("yyyyMM"))
-        // {
-        //     var currentMonth = start;
-        //     var sum = 0;
-        //     var period = new Period(start, end);
-        //     while (currentMonth < new DateTime(end.Year, end.Month, 1).AddMonths(1))
-        //     {
-        //         var budget = GetBudget(budgets, currentMonth.ToString("yyyyMM"));
-        //         if (budget != null)
-        //         {
-        //             sum += budget.GetOverlappingAmount(period);
-        //         }
-        //
-        //         currentMonth = currentMonth.AddMonths(1);
-        //     }
-        //
-        //     return sum;
-        // }
-        // else
-        // {
-        //     var oneMonthBudget = GetBudget(budgets, start.ToString("yyyyMM"));
-        //     if (oneMonthBudget == null) return 0;
-        //
-        //     var amount = oneMonthBudget.Amount;
-        //     var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
-        //     var amountPerDay = amount / startMonthDays;
-        //     return amountPerDay * ((end - start).Days + 1);
-        // }
     }
 
     private static Budget? GetBudget(List<Budget> budgets, string yearMonth)
