d4b8fb0e7dd07033f7d822e7ecbb3cd19bd042f7
Joey Chen <jay.hatelove@gmail.com>
2023-02-11 22:31:30 +0800

    move Period class to another file

--- tdd-with-csharp/BudgetService.cs
+++ tdd-with-csharp/BudgetService.cs
@@ -8,18 +8,6 @@ using System.Linq;
 
 namespace tdd_with_csharp;
 
-public class Period
-{
-    public Period(DateTime start, DateTime end)
-    {
-        Start = start;
-        End = end;
-    }
-
-    public DateTime End { get; private set; }
-    public DateTime Start { get; private set; }
-}
-
 public class BudgetService
 {
     private readonly IBudgetRepo _budgetRepo;
---
+++ tdd-with-csharp/Period.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tdd_with_csharp;
+
+public class Period
+{
+    public Period(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public DateTime End { get; private set; }
+    public DateTime Start { get; private set; }
+}
